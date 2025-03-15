ea6e9e70ad97143e212b36683993abf9c852a267
Harrison Allen <harrisonallen555@gmail.com>
2025-03-15 15:40:57 -0400

    handle post request for login

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -1,9 +1,21 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return jsonify(hello="world")
+def welcome():
+    return jsonify(message="Welcome to Pebbleland!")
+
+@app.route("/login", methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        data = request.get_json()
+        watch_token = data['watch_token']
+        account_token = data['account_token']
+        username = data['username']
+        return jsonify(message=f"Hi {username}! Your account token is {account_token} and your watch token is {watch_token}")
+        # return jsonify({"user": username, "account": account_token, "watch": watch_token})
+    else:
+        return jsonify(message="Get requests not supported")
