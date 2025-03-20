3c6a131c5f7e49792851369a19f52707ec91fef2
Harrison Allen <harrisonallen555@gmail.com>
2025-03-20 18:16:28 -0400

    first multiplayer server side implementation

--- nodeserver/app.js
+++ nodeserver/app.js
@@ -14,7 +14,29 @@ var clicks = 0;
 var last_click = "No one!";
 var users = {};
 
+async function broadcast(to_send) {
+    console.log('Broadcasting: ', to_send);
+    wss.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(to_send);
+        }
+    });
+}
 
+async function get_connected_users() {
+    var user_dict = {}
+    wss.clients.forEach(function each(client) {
+        user_dict[client.username] = {
+            "x": client.x,
+            "y": client.y
+        };
+    });
+    return user_dict;
+}
+
+// TODO:
+// Figure out this error: code: 'WS_ERR_INVALID_CLOSE_CODE',
+//                              [Symbol(status-code)]: 1002
 
 wss.on("connection", async function connection(ws) {
     console.log('New WebSocket connection established. Requesting login...');
@@ -30,24 +52,24 @@ wss.on("connection", async function connection(ws) {
         var response = await handle_message(message, ws);
         var to_send = JSON.stringify(response);
         if (response["close"]) {
-            ws.close(event_codes[response["code"]], to_send);
+            if (ws.readyState === WebSocket.OPEN) { // maybe error is closing on already closed socket?
+                ws.close(event_codes[response["code"]], to_send);
+            }
         } else {
             if (response["broadcast"]) {
-                console.log('Broadcasting: ', to_send);
-                wss.clients.forEach(function each(client) {
-                    if (client.readyState === WebSocket.OPEN) {
-                        client.send(to_send);
-                    }
-                });
+                broadcast(to_send);
             } else {
-                console.log('Sending: ', to_send);
+                console.log('Sending:', to_send);
                 ws.send(to_send);
             }
         }
     });
     
     ws.on("close", async function close() {
-        console.log("Connection closed");
+        console.log("Connection for", ws.username, "closed");
+        var response = {"reason": "user_disconnected", "source": ws.username, "broadcast": true};
+        var to_send = JSON.stringify(response);
+        broadcast(to_send);
         if (users[ws.username]) {
             delete users[ws.username];
         }
@@ -57,7 +79,7 @@ wss.on("connection", async function connection(ws) {
 async function handle_message(data, ws) {
     var message = JSON.parse(data);
     var request = message['request'];
-    var response;
+    var response = {};
     if (request == 'login') {
         response = await login(message);
         if (response["close"]) {
@@ -67,8 +89,6 @@ async function handle_message(data, ws) {
             ws.username = response["username"];
             users[ws.username] = ws;
         }
-        response["clicks"] = clicks;
-        response["source"] = last_click;
     }
     if (request == 'close') {
         response = {'close': true, 'code': event_codes["OK"]};
@@ -83,6 +103,20 @@ async function handle_message(data, ws) {
         }
         last_click = ws.username;
         response = {"reason": "click", "clicks": clicks, "source": last_click, "broadcast": true};
+    }
+    if (request == 'location') {
+        ws.x = message["x"];
+        ws.y = message["y"];
+        response = {"reason": "location", "x": message["x"], "y": message["y"], "source": ws.username, "broadcast": true};
+    }
+    if (request == 'user_connected') {
+        ws.x = message["x"];
+        ws.y = message["y"];
+        response = {"reason": "user_connected", "x": message["x"], "y": message["y"], "source": ws.username, "broadcast": true};
+    }
+    if (request == 'poll_users') {
+        var user_dict = await get_connected_users();
+        response = {"reason": "polled_users", "users": user_dict};
     }
 
     return response
@@ -127,7 +161,6 @@ async function db_run(database, sql, params) {
 }
 
 async function login(message) {
-    
     var watch = message['watch_token'];
     var account = message['account_token'];
     var username = message['username'];
